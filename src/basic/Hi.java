b5b05b8c0749176c4c44e9646cee41efbc6a5a13
naina-is <islam.naina15@gmail.com>
2022-04-12 12:52:11 -0400

    first commit

---
+++ src/basic/Hi.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		String animal = "rabbit";
+		System.out.println(animal);
+	}
+	
+}
---
+++ src/basic/Hi2.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class Hi2 {
+
+	public static void main(String[] args) {
+		String pet = "cat";
+		System.out.println(pet);
+	
+}
+}
